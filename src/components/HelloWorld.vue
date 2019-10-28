3dc7096e730668e0a6cc599b3a2d263c8dbcb25c
kondoumh <kondoumh@gmail.com>
2019-10-29 00:02:34 +0900

    Apply filters

--- src/components/HelloWorld.vue
+++ src/components/HelloWorld.vue
@@ -1,21 +1,41 @@
 
 <template>
-  <v-data-table
-    :headers="headers"
-    :items="posts"
-    :items-per-page="itemsPerPage"
-    :footer-props="{
-      'items-per-page-options': [5, 10, 15, 20, 25, 30]
-    }"
-    class="elevation-1"
-  >
-    <template v-slot:item.date="{ item }">
-      {{ item.date.substring(0, 19) }}
-    </template>
-    <template v-slot:item.slug="{ item }">
-      <a :href="item.url" target="_blank">{{ item.slug ? item.slug : item.url }}</a>
-    </template>
-  </v-data-table>
+  <v-card>
+    <v-card-title>
+      <v-text-field
+        v-model="search"
+        append-icon="search"
+        label="Search"
+        single-line
+        hide-details
+      ></v-text-field>
+    </v-card-title>
+    <v-data-table
+      :headers="headers"
+      :items="posts"
+      :search="search"
+      :items-per-page="itemsPerPage"
+      :footer-props="{
+        'items-per-page-options': [5, 10, 15, 20, 25, 30]
+      }"
+      class="elevation-1"
+    >
+      <template v-slot:item.date="{ item }">
+        {{ item.date.substring(0, 19) }}
+      </template>
+      <template v-slot:item.slug="{ item }">
+        <a :href="item.url" target="_blank">{{ item.slug ? item.slug : item.url }}</a>
+      </template>
+      <template v-slot:body.append>
+        <tr>
+          <td colspan="3"></td>
+          <td>
+            <v-text-field v-model="count" type="number" label="More than"></v-text-field>
+          </td>
+        </tr>
+      </template>
+    </v-data-table>
+  </v-card>
 </template>
 
 <script>
@@ -31,17 +51,28 @@
       }
     },
     computed: {
-      url: () => 'https://compassionate-thompson-b0de67.netlify.com/tumblr-score.json'
+      url: () => 'https://compassionate-thompson-b0de67.netlify.com/tumblr-score.json',
+      headers() {
+        return [
+          { text: 'Date', value: 'date', filterable: false },
+          { text: 'Slug', value: 'slug', sortable: false, filterable: false },
+          { text: 'Type', value: 'type' },
+          {
+            text: 'Count',
+            value: 'count',
+            filter: value => {
+              if (!this.count) return true
+              return value > parseInt(this.count)
+            }
+          }
+        ]
+      },
     },
     data: () => ({
       posts: [],
+      search: "",
+      count: "",
       itemsPerPage: 5,
-      headers: [
-        { text: 'Date', value: 'date' },
-        { text: 'Slug', value: 'slug', sortable: false },
-        { text: 'Type', value: 'type' },
-        { text: 'Count', value: 'count' }
-      ],
     })
   }
 </script>
